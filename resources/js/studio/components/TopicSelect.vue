7a8beb538b9008f1b5227922b61e7ca4ebe62c73
ando-hunterz <ando.hunterz@gmail.com>
2020-06-27 19:32:55 +0700

    testing

--- resources/js/studio/components/TopicSelect.vue
+++ resources/js/studio/components/TopicSelect.vue
@@ -8,6 +8,9 @@
         label="name"
         track-by="slug"
         style="cursor: pointer"
+        :tag-placeholder="trans.app.add_a_new_topic"
+        :taggable="true"
+        @tag="addTopic"
     />
 </template>
 
