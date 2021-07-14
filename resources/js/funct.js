ff93d6c4828a07272c526ec82aeda83009d88d22
julando.omar <julando.omar@student.umn.ac.id>
2021-07-14 19:36:06 +0700

    Refresh Codebase

--- resources/js/funct.js
+++
@@ -1,8 +0,0 @@
-// import $ from 'jquery';
-// require('bootstrap')
-
-// $('#signup-btn').on('click', function(){
-//     console.log($('#registerModal').dropdown('toggle'))
-//     $('#registerModal').modal('show'),
-//     $('#registeremail').val($('#email-signup').val())
-// })
