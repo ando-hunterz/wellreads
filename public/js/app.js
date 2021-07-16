256c5f834a832f0926d46772dab82c6a47adff64
ando-hunterz <ando.huntez@gmail.com>
2021-07-16 21:55:09 +0700

    secure https

--- public/js/app.js
+++ public/js/app.js
@@ -16295,24 +16295,30 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("h1", null, "Hello App!", -1
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("h1", {
+  "class": "text-center bg-yellow-600"
+}, "Hello App!", -1
 /* HOISTED */
 );
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Go to Home");
+var _hoisted_2 = {
+  "class": "flex flex-row justify-between"
+};
 
-var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Go to About");
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Go to Home");
+
+var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Go to About");
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_router_link = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("router-link");
 
   var _component_router_view = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("router-view");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)("div", null, [_hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("p", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" use the router-link component for navigation. "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" specify the link by passing the `to` prop. "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" `<router-link>` will render an `<a>` tag with the correct `href` attribute "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_router_link, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)("div", null, [_hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("nav", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" use the router-link component for navigation. "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" specify the link by passing the `to` prop. "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" `<router-link>` will render an `<a>` tag with the correct `href` attribute "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_router_link, {
     to: "/"
   }, {
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [_hoisted_2];
+      return [_hoisted_3];
     }),
     _: 1
     /* STABLE */
@@ -16321,7 +16327,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     to: "/about"
   }, {
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [_hoisted_3];
+      return [_hoisted_4];
     }),
     _: 1
     /* STABLE */
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <h1>Hello App!</h1>
-        <p>
+        <h1 class="text-center bg-yellow-600">Hello App!</h1>
+        <nav class="flex flex-row justify-between">
             <!-- use the router-link component for navigation. -->
             <!-- specify the link by passing the `to` prop. -->
             <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
             <router-link to="/">Go to Home</router-link>
             <router-link to="/about">Go to About</router-link>
-        </p>
+        </nav>
         <!-- route outlet -->
         <!-- component matched by the route will render here -->
         <router-view></router-view>
